120c2f961e6e628e778d5f5136861cb587849da6
Rahmat Januardi <rahmatjanuardi4@gmail.com>
2025-08-04 10:21:19 +0700

    update desain cetak label release

--- qa_label_cetak.php
+++ qa_label_cetak.php
@@ -25,21 +25,20 @@ $style = array('border' => false);
 $udd = mysql_fetch_assoc(mysql_query("SELECT nama,alamat,telp FROM utd WHERE down='1' AND aktif='1'"));
 
 // Header
-$pdf->SetXY(0,0);
-$pdf->Cell(0, 12,'PALANG MERAH INDONESIA',0, 1, 'C');
+$pdf->SetXY(0, 0);
+$pdf->Cell(0, 12, 'PALANG MERAH INDONESIA', 0, 1, 'C');
 
-$udd=mysql_fetch_assoc(mysql_query("select nama,alamat, telp from utd where down='1' and aktif='1'"));
+$udd = mysql_fetch_assoc(mysql_query("select nama,alamat, telp from utd where down='1' and aktif='1'"));
 $pdf->SetFont('helvetica', 'b', 12);
-$pdf->SetFont('helvetica', 'b', 12);
-$pdf->SetXY(0,5);
-$pdf->Cell(0, 12,$udd[nama],0, 1, 'C');
+$pdf->SetXY(0, 5);
+$pdf->Cell(0, 12, $udd[nama], 0, 1, 'C');
 $pdf->SetFont('helvetica', '', 7);
-$pdf->SetXY(0,14);
-$pdf->Cell(0, 0,$udd[alamat],0, 1, 'C');
-$pdf->Cell(0, 0,'No. Telp. '. $udd[telp],0, 1, 'C');
+$pdf->SetXY(0, 14);
+$pdf->Cell(0, 0, $udd[alamat], 0, 1, 'C');
+$pdf->Cell(0, 0, 'No. Telp. ' . $udd[telp], 0, 1, 'C');
 
 $pdf->SetLineWidth(0.5);
-$pdf->Line(0,22,110,22);
+$pdf->Line(0, 22, 110, 22);
 
 // Info jam ambil
 if ($kantong1['produk'] == 'WE') {
@@ -51,7 +50,8 @@ if ($kantong1['produk'] == 'WB' && $donasi22['jam_ambil']) {
     $ambil1 = date("H:i", strtotime($donasi22['jam_ambil']));
 }
 
-function tglFormat($tgl) {
+function tglFormat($tgl)
+{
     $bulan = array("", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
     $d = date("d", strtotime($tgl));
     $m = date("n", strtotime($tgl));
@@ -62,12 +62,14 @@ function tglFormat($tgl) {
 // Kiri
 $pdf->SetFont('helvetica', 'B', 9);
 $pdf->SetXY(3, 25);
-$pdf->Cell(23, 0, 'No. Kantong', 0);
+$pdf->Cell(20, 0, 'No. Kantong', 0);
+$pdf->SetFont('helvetica', 'B', 11);
 $pdf->Cell(0, 0, ': ' . $_GET['noKantong'], 0);
 
 $pdf->SetFont('helvetica', 'B', 9);
 $pdf->SetXY(3, 30);
-$pdf->Cell(23, 0, 'No. Selang', 0);
+$pdf->Cell(20, 0, 'No. Selang', 0);
+$pdf->SetFont('helvetica', 'B', 11);
 $pdf->Cell(0, 0, ': ' . $kantong1['noSelang'], 0);
 
 $pdf->SetXY(4, 35);
@@ -86,14 +88,14 @@ $pdf->Cell(0, 0, ': ' . tglFormat($kantong1['tglpengolahan']), 0);
 $pdf->SetXY(3, 59);
 $pdf->Cell(23, 0, 'Tgl. Kedaluwarsa', 0);
 // $pdf->Cell(0, 0, ': ' . tglFormat($kantong1['kadaluwarsa']), 0);
-$pdf->Cell(0, 0, ': ' . tglFormat($kantong1['kadaluwarsa']).'  ' .$ambil1, 0);
+$pdf->Cell(0, 0, ': ' . tglFormat($kantong1['kadaluwarsa']) . '  ' . $ambil1, 0);
 
 $pdf->SetXY(3, 67);
 $pdf->SetFont('helvetica', 'B', 8);
 $pdf->Cell(23, 0, 'Produk', 0);
 $pdf->Cell(0, 0, ': ', 0);
-$pdf->SetXY(3, 72);
-$pdf->SetFont('helvetica', 'B', 25);
+$pdf->SetXY(3, 70);
+$pdf->SetFont('helvetica', 'B', 35);
 $pdf->Cell(0, 5, $kantong1['produk'], 0);
 $pdf->SetXY(3, 85);
 $pdf->SetFont('helvetica', '', 8);
@@ -102,7 +104,7 @@ $pdf->Cell(0, 0, ': ' . $kantong1['volume'] . ' cc', 0);
 $pdf->SetFont('helvetica', '', 8);
 $pdf->SetXY(3, 90);
 $pdf->Cell(23, 0, 'Suhu Simpan', 0);
-$pdf->Cell(0, 0, ': 2-6°C', 0);
+$pdf->Cell(0, 0, ': 2-6Â°C', 0);
 
 // Kanan
 $pdf->SetXY(55, 25);
@@ -130,16 +132,16 @@ $pdf->SetXY(55, 81);
 $pdf->Cell(0, 0, 'Status:', 0);
 $pdf->SetXY(56, 86);
 $pdf->SetFont('helvetica', 'B', 20);
-if($kantong1['hasil_release'] == '2') {
-	$pdf->SetFillColor(0, 0, 0); // hitam
-	$pdf->SetTextColor(255, 255, 255); // merah
-	$pdf->Rect(56, 86, 45, 10, 'DF');
-	$pdf->Cell(45, 10, 'R E J E C T', 0, 0, 'C', 0, '', 1);
+if ($kantong1['hasil_release'] == '2') {
+    $pdf->SetFillColor(0, 0, 0); // hitam
+    $pdf->SetTextColor(255, 255, 255); // merah
+    $pdf->Rect(56, 86, 45, 10, 'DF');
+    $pdf->Cell(45, 10, 'R E J E C T', 0, 0, 'C', 0, '', 1);
 } else {
-$pdf->SetFillColor(255, 255, 255); // putih
-	$pdf->SetTextColor(0, 0, 0); // hitam
-	$pdf->Rect(56, 86, 45, 10, 'DF');
-	$pdf->Cell(45, 10, 'R E L E A S E D', 0, 0, 'C', 0, '', 1);
+    $pdf->SetFillColor(255, 255, 255); // putih
+    $pdf->SetTextColor(0, 0, 0); // hitam
+    $pdf->Rect(56, 86, 45, 10, 'DF');
+    $pdf->Cell(45, 10, 'R E L E A S E D', 0, 0, 'C', 0, '', 1);
 }
 
 $pdf->Output('label_kantong.pdf', 'I');
